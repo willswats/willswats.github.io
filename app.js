9d54e4324123cf844ee5eebd42fcaa02780c99f3
William Watson <williamstuwatson@gmail.com>
2021-08-11 15:20:36 +0100

    Add toggle mobile menu

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ const activateMobileMenu = () => {
     navMenu.classList.toggle('drop-down')
 }
 mobileMenu.addEventListener('click', activateMobileMenu)
+
+const disableMobileMenu = () => {
+    const cross = document.querySelector('.cross')
+    if (window.innerWidth <= 960 && cross) {
+        mobileMenu.classList.toggle('cross')
+        navMenu.classList.toggle('drop-down')
+    }
+}
+navMenu.addEventListener('click', disableMobileMenu)
